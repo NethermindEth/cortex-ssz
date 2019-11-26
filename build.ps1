6e847e5de3c12e84bd2c5d888fb5c3304c3d4816
Sly Gryphon <sgryphon@computer.org>
2019-11-26 22:52:15 +1000

    Use major.minor for assembly version; slight change in informational build metadata format

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@
     if (!$?) { throw 'Tests failed' }
 }
 $v = (dotnet ./tools/gitversion/GitVersion.dll | ConvertFrom-Json)
-dotnet pack src/Cortex.SimpleSerialize -c Release -p:InformationalVersion=$($v.SemVer)+$($v.ShortSha) -p:Version=$($v.SemVer) -p:NuGetVersion=$($v.NuGetVersion)
+dotnet pack src/Cortex.SimpleSerialize -c Release -p:AssemblyVersion=$($v.AssemblySemVer) -p:FileVersion=$($v.AssemblySemFileVer) -p:Version=$($v.SemVer)+$($v.CommitsSinceVersionSource).Sha.$($v.ShortSha) -p:PackageVersion=$($v.NuGetVersion)
